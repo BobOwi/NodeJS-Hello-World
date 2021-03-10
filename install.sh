69ab74be5fe3d470041406f534d73c1553d9ad6c
BobOwi <bobthebiker@gmail.com>
2021-03-10 19:09:00 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 apt update
 apt install -y nodejs npm git
 git clone https://github.com/BobOwi/NodeJS-Hello-World.git
-npm install express
+npm install express ejs
 cd NodeJS-Hello-World
 node app.js
