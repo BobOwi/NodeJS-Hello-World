65098659156c3dbd85d148cd64f3b29cdb2724d7
BobOwi <bobthebiker@gmail.com>
2021-03-10 19:07:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 apt update
 apt install -y nodejs npm git
-git clone https://github.com/BobOwi/
-cd flask-mqtt
+git clone https://github.com/BobOwi/NodeJS-Hello-World.git
 npm install express
-
+cd NodeJS-Hello-World
 node app.js
