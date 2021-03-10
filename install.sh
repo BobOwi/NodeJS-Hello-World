12204993d0c9c101358305ce150d78b4421bdddc
BobOwi <bobthebiker@gmail.com>
2021-03-10 19:22:54 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 curl -fsSL https://deb.nodesource.com/setup_15.x | sudo -E bash -
-apt install -y nodejs npm git
-git clone https://github.com/BobOwi/NodeJS-Hello-World.git
+curl -sL https://deb.nodesource.com/setup_15.x | bash -
+apt install -y nodejs git
 npm install express ejs
+git clone https://github.com/BobOwi/NodeJS-Hello-World.git
 cd NodeJS-Hello-World
 node app.js
